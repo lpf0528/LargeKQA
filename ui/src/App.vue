f85efd5c5b1dad9b4ce8e7c7b14377a2fc3e7f1a
pengfei.liu <pengfei.liu@tenclass.com>
2024-12-18 20:24:00 +0800

    init ui project

---
+++ ui/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view /> <!-- 用于渲染当前路由的组件 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+/* 全局样式 */
+</style>
